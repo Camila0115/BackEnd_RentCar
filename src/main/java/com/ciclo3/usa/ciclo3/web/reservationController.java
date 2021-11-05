1c982b5f08c963b09a441ce04c9e93e640dc7e31
Huver Andrey Galindo Salgado <hgalindo@inovait.co>
2021-11-05 12:34:12 -0500

    ajuste cros

--- src/main/java/com/ciclo3/usa/ciclo3/web/reservationController.java
+++ src/main/java/com/ciclo3/usa/ciclo3/web/reservationController.java
@@ -19,9 +19,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api/Reservation")
+@CrossOrigin(origins = "*", methods={RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
 public class reservationController {
 
     @Autowired
