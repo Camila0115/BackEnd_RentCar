59721326fbe54ebef55dccbc3c1ee493de27bd33
Camila0115 <camila.onate.mt@correo.usa.edu.co>
2021-11-24 21:51:25 -0500

    Se borra todo

--- src/main/java/com/ciclo3/usa/ciclo3/repository/messageCrudRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ciclo3.usa.ciclo3.repository;
-
-import com.ciclo3.usa.ciclo3.model.Message;
-import org.springframework.data.repository.CrudRepository;
-
-public interface messageCrudRepository extends CrudRepository<Message, Integer> {
-  
-}
-
